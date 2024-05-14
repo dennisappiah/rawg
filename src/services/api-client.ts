e0aa4b18ce34e2bb7ffa9999e48a78ee6a6260c4
dennisappiah <appiahdennis1998@gmail.com>
2024-05-14 15:03:59 +0000

    services

---
+++ src/services/api-client.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+export interface FetchResponse<T> {
+  count: number;
+  next: string | null;
+  results: T[];
+}
+
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "e7d0dd5b5c5140a190965fd754245f8a",
+  },
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: false,
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
